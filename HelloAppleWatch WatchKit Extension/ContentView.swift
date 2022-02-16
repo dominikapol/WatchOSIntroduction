c579e1a7582559278d536f835bf45fadd992ce49
Dominika <dominikapiganova@mail.ru>
2022-02-16 16:11:41 +0200

    Getting Started with watchOS

--- HelloAppleWatch WatchKit Extension/ContentView.swift
+++ HelloAppleWatch WatchKit Extension/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var sentence = EmojiSentence()
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+           Image("Cookie")
+               .resizable()
+               .scaledToFit()
+               .overlay(
+                Text(sentence.emoji)
+                    .font(.title3)
+                    .padding(.top, 10)
+                    .buttonStyle(.plain)
+               )
+            Text(sentence.text)
+                .font(.caption)
+                .padding(.top, 20)
+        } .onTapGesture {
+            sentence.next()
+        }
     }
 }
 
