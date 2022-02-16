ea984c957bf6bbec3a53faf4272da5af760e1b10
Dominika <dominikapiganova@mail.ru>
2022-02-16 11:47:05 +0200

    Initial Commit

---
+++ HelloAppleWatch WatchKit Extension/HelloAppleWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  HelloAppleWatchApp.swift
+//  HelloAppleWatch WatchKit Extension
+//
+//  Created by Dominika Poleshyck on 16.02.22.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppleWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
